738e11d4182ac73bbba513ad51ccb53e61b6a02e
Pooja Kale <pjakale@amazon.com>
2026-01-27 14:48:55 -0800

    Add node connectors
    Transferring contents of aurora-dsql-node-connector repo to the
    node folder of connectors.

---
+++ node/packages/postgres-js/src/index.ts
@@ -0,0 +1,6 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+export { auroraDSQLPostgres, auroraDSQLWsPostgres } from './client.js';
+export type { AuroraDSQLConfig, AuroraDSQLWsConfig } from './client.js';
