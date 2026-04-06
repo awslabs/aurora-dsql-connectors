c1573b47128d74f0fb4778b563770d1882b88a3f
Aleksandar Maksimovic <amaksimovic3@hotmail.com>
2026-04-06 14:58:06 -0700

    Enable feature-gated modules in docs.rs
    Adds docs.rs metadata to build documentation with all features enabled,
    allowing the pool and occ modules to appear in published docs. Also adds
    doc_cfg attribute to automatically annotate feature-gated items with
    their required features.
    
    Changes:
    - Cargo.toml: Add [package.metadata.docs.rs] with all-features = true
    - lib.rs: Add #![cfg_attr(docsrs, feature(doc_cfg))] for auto annotations

--- rust/sqlx/src/lib.rs
+++ rust/sqlx/src/lib.rs
@@ -1,6 +1,8 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
+#![cfg_attr(docsrs, feature(doc_cfg))]
+
 //! Aurora DSQL connector for SQLx.
 
 mod config;
